27e247e0ea27e3d5cbbb84ef01b679092b8bc200
sarthakbombale <sarthakbomble250@gmail.com>
2026-02-13 22:11:50 +0530

    server side and client side rendering

--- app/components/hello.tsx
+++ app/components/hello.tsx
@@ -1,1 +1,12 @@
-'use client'
+
+import React from 'react'
+
+const Hello = () => {
+    console.log("i am client compontents")
+
+    return (
+        <div>hello i am client compontent</div>
+    )
+}
+
+export default Hello
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Hello from './components/hello'
 const Home = () => {
+  console.log("what type of componets ")
+
   return (
-    <div className='5x1 underline'> Welcome to Home</div>
+    <main>
+
+      <div className='5x1 underline'> Welcome to Home</div>
+      <Hello />
+    </main>
   )
 }
 
